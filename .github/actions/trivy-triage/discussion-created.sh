9fe70eed4d036d2187294ea9c4d24c6c13ded743
Itay Shakury <itay@itaysk.com>
2023-09-14 14:15:29 +0300

    config file

--- .github/actions/trivy-triage/discussion-created.sh
+++ .github/actions/trivy-triage/discussion-created.sh
@@ -1,16 +1,23 @@
+#! /usr/bin/env bash
 set -x
+
 # acquire context
 discussion_created_json="$1"
 discussion_category_name=$(jq -r '.category.name' "$discussion_created_json")
 discussion_node_id=$(jq -r '.node_id' "$discussion_created_json")
 discussion_body=$(jq -r '.body' "$discussion_created_json")
+config_discussion_labels=${{ inputs.config_discussion_labels_path }}
+if ! [ -f "$config_discussion_labels" ]; then
+  echo "invalid discussion labels config file"
+  exit 1
+fi
 
 # find labels in discussion body
 if [ "$discussion_category_name" != 'Ideas' ]; then exit 0; fi
 discussion_target=$(awk 'BEGIN{ RS="\\\\n\\\\n" } /^### Target/ {getline; print;}' <<< "$discussion_body")
 discussion_scanner=$(awk 'BEGIN{ RS="\\\\n\\\\n" } /^### Scanner/ {getline; print;}' <<< "$discussion_body")
-label_target=$(awk "/$discussion_target/ {print $2}" <config)
-label_scanner=$(awk "/$discussion_scanner/ {print $2}" <config)
+label_target=$(awk "/$discussion_target/ {print $2}" <"$config_discussion_labels")
+label_scanner=$(awk "/$discussion_scanner/ {print $2}" <"$config_discussion_labels")
 
 # apply labels to discussion
 # TODO: apply all labels in one call
