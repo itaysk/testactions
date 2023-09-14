069e9ef50d6c8b94e0501fe34395c305946685ca
Itay Shakury <itay@itaysk.com>
2023-09-14 13:52:33 +0300

    jq raw

--- .github/actions/trivy-triage/discussion-created.sh
+++ .github/actions/trivy-triage/discussion-created.sh
@@ -1,12 +1,12 @@
 set -x
 # acquire context
 discussion_created_json="$1"
-discussion_category_name=$(jq '.category.name' "$discussion_created_json")
-discussion_node_id=$(jq '.node_id' "$discussion_created_json")
-discussion_body=$(jq '.body' "$discussion_created_json")
+discussion_category_name=$(jq -r '.category.name' "$discussion_created_json")
+discussion_node_id=$(jq -r '.node_id' "$discussion_created_json")
+discussion_body=$(jq -r '.body' "$discussion_created_json")
 
 # find labels in discussion body
-if [ "$discussion_category_name" != "Ideas" ]; then exit 0; fi
+if [ "$discussion_category_name" != 'Ideas' ]; then exit 0; fi
 discussion_target=$(awk 'BEGIN{ RS="\\\\n\\\\n" } /^### Target/ {getline; print;}' <<< "$discussion_body")
 discussion_scanner=$(awk 'BEGIN{ RS="\\\\n\\\\n" } /^### Scanner/ {getline; print;}' <<< "$discussion_body")
 label_target=$(awk "/$discussion_target/ {print $2}" <config)
