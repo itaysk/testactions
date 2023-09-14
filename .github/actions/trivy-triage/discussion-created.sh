d74d259922d498ab5e14f817457cf17ed55b03fe
Itay Shakury <itay@itaysk.com>
2023-09-14 14:24:26 +0300

    embedded config

--- .github/actions/trivy-triage/discussion-created.sh
+++ .github/actions/trivy-triage/discussion-created.sh
@@ -3,14 +3,11 @@ set -x
 
 # acquire context
 discussion_created_json="$1"
+config_discussion_labels="$2"
+
 discussion_category_name=$(jq -r '.category.name' "$discussion_created_json")
 discussion_node_id=$(jq -r '.node_id' "$discussion_created_json")
 discussion_body=$(jq -r '.body' "$discussion_created_json")
-config_discussion_labels=${{ inputs.config_discussion_labels_path }}
-if ! [ -f "$config_discussion_labels" ]; then
-  echo "invalid discussion labels config file"
-  exit 1
-fi
 
 # find labels in discussion body
 if [ "$discussion_category_name" != 'Ideas' ]; then exit 0; fi
